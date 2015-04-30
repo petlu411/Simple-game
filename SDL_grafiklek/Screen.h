3adab60fed91ed511cb169eeab1edfa75e4ff7db
petlu411 <petrus.lundgren@gmail.com>
2015-04-30 20:03:13 +0800

    grunden

---
+++ SDL_grafiklek/Screen.h
@@ -0,0 +1,25 @@
+#pragma once
+#include"SDL.h"
+#include<iostream>
+class Screen
+{
+public:
+	static const int SCREEN_WIDTH = 800;
+	static const int SCREEN_HEIGHT = 600;
+private:
+	SDL_Window *m_window;
+	SDL_Renderer *m_renderer;
+	SDL_Texture *m_texture;
+	Uint32 *m_buffer1;
+	Uint32 *m_buffer2;
+public:
+	Screen();
+	bool init();
+	bool processEvents();
+	void boxBlur();
+	void close();
+	void update();
+	void setPixel(int x, int y, Uint8 red, Uint8 green, Uint8 blue);
+	~Screen();
+};
+
